59f679bfe617e94c3db75be64f510630a2c29d2e
Code-writer130 <nanamarfo860@gmail.com>
2024-11-11 16:47:41 +0100

    "Rename MainComponent to MainContent and remove MainComponent file"

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
-import MainComponent from "./components/MainComponent";
+import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <WelcomeMessage />
       <Header />
-      <MainComponent />
+      <MainContent />
       <Footer />
     </>
   );
