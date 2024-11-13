610731a8c3c2bc7f06302be39107d3bb05e58d1e
Code-writer130 <nanamarfo860@gmail.com>
2024-11-13 22:58:05 +0100

    counter done

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
