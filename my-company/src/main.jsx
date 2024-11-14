e637e3ffe793d13bc1fb805f4fc852d67dae5d57
Code-writer130 <nanamarfo860@gmail.com>
2024-11-14 19:39:32 +0100

    changed the file names

--- my-company/src/main.jsx
+++ my-company/src/main.jsx
@@ -1,31 +1,35 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  createRoutesFromElements,
-} from "react-router-dom";
-import "./index.css";
-import Layout from "./Layout";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Services from "./components/Services";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="About" element={<About />} />
-      <Route path="Contact" element={<Contact />} />
-      <Route path="Services" element={<Services />} />
-    </Route>
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
-);
+}
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+export default App
