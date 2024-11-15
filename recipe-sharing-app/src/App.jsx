cde47a21cab9e247d6eefdc432b5808ff468bedc
Code-writer130 <nanamarfo860@gmail.com>
2024-11-15 01:28:41 +0100

    initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import RecipeList from "../components/RecipeList";
+import AddRecipeForm from "../components/AddRecipeForm";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
