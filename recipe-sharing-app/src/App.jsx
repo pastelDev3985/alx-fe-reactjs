95d020e05f6a1e49811652563b87ea1da793c893
Code-writer130 <nanamarfo860@gmail.com>
2024-11-16 19:23:50 +0100

    added router to app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import RecipeDetails from "./components/RecipeDetails";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipedetails" element={<RecipeDetails />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipedetails" element={<RecipeDetails />} />
+        </Routes>
+      </Router>
     </>
   );
 }
