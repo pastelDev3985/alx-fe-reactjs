413d198e60a72145d1fb7391ff3bb164ce4b7c8a
Code-writer130 <nanamarfo860@gmail.com>
2024-11-16 19:49:40 +0100

    navigation related

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import RecipeDetails from "./components/RecipeDetails";
 import { Routes, Route, Router } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/EditRecipeForm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipedetails" element={<RecipeDetails />} />
+          <Route path="/addrecipe" element={<AddRecipeForm />} />
         </Routes>
       </Router>
     </>
