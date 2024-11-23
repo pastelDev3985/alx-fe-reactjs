b9887a25543ffc9f6afd78e7c9ddd78d5f51ec9e
Code-writer130 <nanamarfo860@gmail.com>
2024-11-23 23:45:35 +0100

    nested in the profile file

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, Routes, Route } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const profile = () => {
   const { logout } = useAuth();
@@ -17,8 +19,11 @@ const profile = () => {
       <h1>Profile Page</h1>
       <p>Welcome to your profile!</p>
       <button onClick={logout}>Logout</button>
-      {/* Renders child components */}
-      <Outlet />
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
