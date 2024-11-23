fe39865dcda8268aaac3f3912963040013713517
Code-writer130 <nanamarfo860@gmail.com>
2024-11-23 22:26:49 +0100

    profile settings updated

---
+++ react-router-advanced/src/components/Profile/ProfileSettings.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const ProfileSettings = () => {
+  return (
+    <div>
+      <h2>Profile Settings</h2>
+      <p>This is where the user profile settings are managed.</p>
+    </div>
+  );
+};
+
+export default ProfileSettings;
