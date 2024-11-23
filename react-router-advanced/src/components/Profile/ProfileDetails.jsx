940a0bbf71200c65f1fc66dcc0d1bfcb62f7300c
Code-writer130 <nanamarfo860@gmail.com>
2024-11-23 22:25:16 +0100

    profile details component

---
+++ react-router-advanced/src/components/Profile/ProfileDetails.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const ProfileDetails = () => {
+  return (
+    <div>
+      <h2>Profile Details</h2>
+      <p>This is where user profile details are displayed.</p>
+    </div>
+  );
+};
+
+export default ProfileDetails;
