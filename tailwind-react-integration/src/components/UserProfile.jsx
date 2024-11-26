2a90bd75995ec04ad344211abaa5c3cc0ab68b3c
Code-writer130 <nanamarfo860@gmail.com>
2024-11-26 21:45:15 +0100

    hover

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8  mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm">
+    <div className="bg-gray-100 p-8  mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lgtext-sm md:text-xltext-base">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lgtext-sm md:text-xltext-base hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-gray-600 text-base">
