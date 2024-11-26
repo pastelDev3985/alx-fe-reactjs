364d2d9a0c9c8c27a5ea8bb24f1c4e4bde0a4ea0
Code-writer130 <nanamarfo860@gmail.com>
2024-11-26 14:34:08 +0100

    responsivity included

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-8  mx-auto my-20 rounded-lg shadow-lg sm:p-4max-w-xs md:p-8max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24h-24 md:w-36h-36"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lgtext-sm md:text-xltext-base">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
